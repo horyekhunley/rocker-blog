2c3b56d5f6d1ae5e6c29c1fcfd36a75c17730fd8
Mahbub Bello <mahbubbello@gmail.com>
2022-01-02 13:22:01 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const ejs = require('ejs')
+const paginate = require('express-paginate')
+const passport = require('passport')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log(`MongoDB connected...`)
+}).catch((err) => {
+  console.log('Error connecting to MongoDB. Error...', err)
+  process.exit
+})
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+app.use(passport.initialize())
+app.use(paginate.middleware(process.env.LIMIT, process.env.MAX_LIMIT))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
