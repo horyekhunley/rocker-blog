cf390505685eb6f0685964a52544f6fc62ae1ffc
Mahbub Bello <mahbubbello@gmail.com>
2022-01-03 21:05:09 +0100

    implemented admin routes

---
+++ routes/adminRoutes.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const { ensureAuthenticated, ensureAuthorized } = require("../middleware/authMiddleware");
+const { register } = require("../controllers/authCtrl");
+
+const router = express.Router();
+
+const { getAll, getOne } = require("../controllers/adminCtrl");
+
+router.get("/users", ensureAuthenticated, ensureAuthorized(["admin"]), async(req, res) => {
+    /*
+        #swagger.tags = ['Admin']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
+    await getAll(req, res);
+});
+
+router.get("/users/:id", ensureAuthenticated, ensureAuthorized(["admin"]), async(req, res) => {
+
+    /*
+        #swagger.tags = ['Admin']
+        #swagger.security = [{
+            "Authorization": []
+        }]
+    */
+    await getOne(req, res);
+});
+
+router.get("/seed", async(req, res) => { 
+        //#swagger.tags = ['Admin'] 
+    const admin = {
+        name: "Administrator",
+        email: "admin@mahbubbello.com",
+        password: "Password@123"
+    };
+
+    await register(admin, "admin", res);
+});
+
+module.exports = router;
---
+++ routes/authRoutes.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const {validationRules, validate} = require("../validations/userValidator");
+const { login, register, verify, forgotPassword, resetPassword, changePassword } = require("../controllers/authCtrl");
+const { ensureAuthenticated } = require("../middleware/auth-middleware");
+const { validationRules: passwordValidationRules, validate: passwordValidate } = require("../validations/changePasswordValidator");
+
+const router = express.Router();
+
+router.post('/login', async (req, res) => {
+    /*  #swagger.tags = ['Auth']
+    	#swagger.parameters['obj'] = {
+            in: 'body',
+            required: true,
+            schema: { $ref: "#/definitions/LoginModel" }
+    } */
+    await login(req.body, res);
+});
+
+router.post('/register', validationRules(), validate, async (req, res) => {
+    /*  #swagger.tags = ['Auth']
+    	#swagger.parameters['obj'] = {
+            in: 'body',
+            required: true,
+            schema: { $ref: "#/definitions/RegisterModel" }
+    } */
+
+    await register(req.body, "user", res);
+});
+
+router.post('/verify', async (req, res) => {
+    /*  #swagger.tags = ['Auth']
+    	#swagger.parameters['obj'] = {
+            in: 'body',
+            required: true,
+            schema: { $ref: "#/definitions/VerifyEmailModel" }
+    } */
+    await verify(req.body, res);
+});
+
+router.post('/forgotPassword', async (req, res) => {
+    /*  #swagger.tags = ['Auth']
+    	#swagger.parameters['obj'] = {
+            in: 'body',
+            required: true,
+            schema: { $ref: "#/definitions/ForgotPassWordModel" }
+    } */
+    await forgotPassword(req.body, res);
+});
+
+router.post('/resetPassword', passwordValidationRules(), passwordValidate, async (req, res) => {
+    /*  #swagger.tags = ['Auth']
+    	#swagger.parameters['obj'] = {
+            in: 'body',
+            required: true,
+            schema: { $ref: "#/definitions/ResetPasswordModel" }
+    } */
+    await resetPassword(req.body, res);
+});
+
+router.post("/changePassword", ensureAuthenticated, passwordValidationRules(), passwordValidate, async (req, res) => {   
+    /*  #swagger.tags = ['Auth']
+        #swagger.security = [{
+        "Authorization": []
+        }]
+    	#swagger.parameters['obj'] = {
+            in: 'body',
+            required: true,
+            schema: { $ref: "#/definitions/ChangePasswordModel" }
+    } */   
+    
+    await changePassword(req, res);
+});
+
+module.exports = router;
